dc2e70feb4490e9179ad670ddaaea87a774bee22
aishwaryatesteng <aishwaryatesteng7@gmail.com>
2025-09-14 18:59:43 +0530

    day1

---
+++ chrome_assignment1.java
@@ -0,0 +1,22 @@
+package Week.Day1;
+
+public class chrome_assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float value=91.0f;
+		String s="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		System.out.println("The float value is: "+value);
+		System.out.println("The string value is: "+s);
+		System.out.println("The isBeta value is: "+isBeta);
+		System.out.println("The release year is: "+releaseYear);
+		System.out.println("The shortcutey is: "+shortcutKey);
+		
+		
+
+	}
+
+}
